e90e85cde68a1f44bde636bf10b5e21889908386
gthoma17 <gthoma17@emich.edu>
2015-09-14 17:17:17 -0400

    Fixed update scripts for *nix

--- devops/linux/updateServer.sh
+++ devops/linux/updateServer.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Updating remote server -- pulling from github"
-cd "$(dirname "$0")"
+cd $(dirname $(readlink -f $0))
 ssh -i bin/identity.ppk 481_user@gat.im /apps/cosc481/devops/server/cloneGit.sh
